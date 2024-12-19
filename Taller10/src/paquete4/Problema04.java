eb485cab6324f6c63bf20e7a8e010ce6b3dc8a8a
Emilio <ecueva611@gmail.-com>
2024-12-19 12:50:59 -0500

    cambios

--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -5,10 +5,35 @@
  */
 package paquete4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema04 {
-    
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        double[][] datos = new double[2][2];
+        int fila;
+        int col;
+        double valor;
+        int contador = 0;
+
+        for (fila = 0; fila < 2; fila++) {
+            for (col = 0; col < 2; col++) {
+                System.out.println("Ingrese un valor");
+                valor = entrada.nextDouble();
+                if(valor > 1000 && valor < 1199){
+                    datos[fila][col] = valor;
+                } else {
+                    datos[fila][col] = 10;
+                    contador = contador +1;
+                }
+                
+            }
+        }
+    }
 }
