def69d0fc09a8c4356a013e9f4ce41e196dba56a
Emilio Jose Cueva <ecueva611@gmail.com>
2024-12-19 21:40:47 -0500

    cambios

--- Taller10/src/paquete4/Problema04.java
+++ Taller10/src/paquete4/Problema04.java
@@ -16,24 +16,34 @@ public class Problema04 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        double[][] datos = new double[2][2];
+        int[][] datos = new int[2][2];
         int fila;
         int col;
-        double valor;
+        int valor;
         int contador = 0;
 
         for (fila = 0; fila < 2; fila++) {
             for (col = 0; col < 2; col++) {
                 System.out.println("Ingrese un valor");
-                valor = entrada.nextDouble();
-                if(valor > 1000 && valor < 1199){
+                valor = entrada.nextInt();
+                if (valor > 1000 && valor < 1199) {
                     datos[fila][col] = valor;
                 } else {
                     datos[fila][col] = 10;
-                    contador = contador +1;
+                    contador = contador + 1;
                 }
-                
+
             }
         }
+        for (fila = 0; fila < 2; fila++) {
+            for (col = 0; col < 2; col++) {
+                System.out.printf("El arreglo en posición [%d][%d] es: %d\n",
+                        fila,
+                        col,
+                        datos[fila][col]);
+            }
+        }
+        System.out.printf("veces que se agregó un valor fuera de rango: %d",
+                contador);
     }
 }
