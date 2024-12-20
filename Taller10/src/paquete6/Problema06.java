cd093ac783956a6b64914acf8a432f349dde9248
Emilio Jose Cueva <ecueva611@gmail.com>
2024-12-19 21:58:04 -0500

    cambios

--- Taller10/src/paquete6/Problema06.java
+++ Taller10/src/paquete6/Problema06.java
@@ -10,5 +10,28 @@ package paquete6;
  * @author reroes
  */
 public class Problema06 {
-    
+
+    public static void main(String[] args) {
+        String[][] estudiantes = {
+            {"Sarah Ward", "Philip Payne"},
+            {"Carrie Burton", "Lauren Rice"},
+            {"Emma Escobar", "Lori Flores"},
+            {"Steven West", "Toni Martin"}
+        };
+
+        String acumulador = "";
+
+        for (int i = 0; i < estudiantes.length; i++) {
+            for (int j = 0; j < estudiantes[i].length; j++) {
+
+                if (estudiantes[i][j].length() == 11) {
+                    acumulador = String.format("%s%s\n",
+                            acumulador,
+                            estudiantes[i][j]);
+                }
+            }
+        }
+        System.out.printf("Estudiantes con 11 caracteres:\n%s", acumulador);
+    }
+
 }
